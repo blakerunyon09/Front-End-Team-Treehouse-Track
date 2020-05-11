d83ec2ec9820216a04b67d4dc44cc2b66ba4d779
blakerunyon09 <65094249+blakerunyon09@users.noreply.github.com>
2020-05-11 11:10:41 -0600

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,11 @@
+// Pop up box with text.
+
+alert("What up B?");
+
+// Pop up box with question.
+
+prompt("What's up G-Money?");
+
+// Write to the HTML
+
+document.write("<p>What's up Bobby?</p>");
